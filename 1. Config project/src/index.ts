c8dacdaf59a745f99a88a5565f05f9972df081c1
frontendLearning-31082022 <MorglHer@yandex.ru>
2024-07-01 00:26:14 +0300

    ESLint injected

--- 1. Config project/src/index.ts
+++ 1. Config project/src/index.ts
@@ -4,11 +4,11 @@ import { renderUserBlock } from './user.js'
 import { renderToast } from './lib.js'
 
 window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock('0')
-  renderSearchFormBlock()
-  renderSearchStubBlock()
-  renderToast(
-      {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
-      {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
-  )
+    renderUserBlock('0')
+    renderSearchFormBlock()
+    renderSearchStubBlock()
+    renderToast(
+        { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
+        { name: 'Понял', handler: () => { console.log('Уведомление закрыто') } }
+    )
 })
