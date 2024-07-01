5a150741c7ea5312976910b0447c2525cc5508ba
frontendLearning-31082022 <MorglHer@yandex.ru>
2024-07-01 21:43:10 +0300

    solved tasks 2,3

--- 1. Config project/src/search-form.ts
+++ 1. Config project/src/search-form.ts
@@ -1,9 +1,36 @@
 import { renderBlock } from './lib.js'
 
-export function renderSearchFormBlock () {
-  renderBlock(
-    'search-form-block',
-    `
+export function renderSearchFormBlock() {
+    const minDate: Date = new Date();
+    const minDate_out: Date = new Date(new Date(minDate.getTime()).setDate(new Date(minDate.getTime()).getDate() + 2));
+
+    let maxDate: Date = new Date();
+    maxDate.setMonth(maxDate.getMonth() + 1);
+    maxDate = new Date(new Date(maxDate.getTime()).getFullYear(), new Date(maxDate.getTime()).getMonth() + 1, 0);
+    const maxDate_out: Date = new Date(new Date(maxDate.getTime()).setDate(new Date(maxDate.getTime()).getDate() + 2));
+
+    const tomorrow: any = new Date(new Date().setDate(new Date().getDate() + 1));
+    const tomorrow_plus2: any = new Date(new Date().setDate(new Date().getDate() + 3));
+
+    function dateToStr(date: Date) {
+        return date.toLocaleDateString("fr-CA");
+    }
+
+    const reCalc_out = document.createElement('script');
+    reCalc_out.innerHTML = `
+            function reCalc_out(e){
+                choosedDate=new Date( Date.parse(e.value));
+                const minIncrement = new Date(choosedDate.setDate(choosedDate.getDate() + 2));
+                document.getElementById('check-out-date').min=minIncrement.toLocaleDateString("fr-CA")
+                document.getElementById('check-out-date').value=minIncrement.toLocaleDateString("fr-CA")
+                debugger;
+            }
+       `;
+    document.body.appendChild(reCalc_out);
+
+    renderBlock(
+        'search-form-block',
+        `
     <form>
       <fieldset class="search-filedset">
         <div class="row">
@@ -20,11 +47,11 @@ export function renderSearchFormBlock () {
         <div class="row">
           <div>
             <label for="check-in-date">Дата заезда</label>
-            <input id="check-in-date" type="date" value="2021-05-11" min="2021-05-11" max="2021-06-30" name="checkin" />
+            <input id="check-in-date" type="date" onchange="reCalc_out(this)" value="${dateToStr(tomorrow)}" min="${dateToStr(minDate)}" max="${dateToStr(maxDate)}" name="checkin" />
           </div>
           <div>
             <label for="check-out-date">Дата выезда</label>
-            <input id="check-out-date" type="date" value="2021-05-13" min="2021-05-11" max="2021-06-30" name="checkout" />
+            <input id="check-out-date" type="date" value="${dateToStr(tomorrow_plus2)}" min="${dateToStr(minDate_out)}" name="checkout" />
           </div>
           <div>
             <label for="max-price">Макс. цена суток</label>
@@ -37,5 +64,5 @@ export function renderSearchFormBlock () {
       </fieldset>
     </form>
     `
-  )
+    )
 }
--- 1. Config project/src/user.ts
+++ 1. Config project/src/user.ts
@@ -1,21 +1,21 @@
 import { renderBlock } from './lib.js'
 
-export function renderUserBlock (favoriteItemsAmount) {
-  const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 'ничего нет'
-  const hasFavoriteItems = favoriteItemsAmount ? true : false
+export function renderUserBlock(userName: string, avatarLink: string, favoriteItemsAmount: number) {
+    const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 'ничего нет'
+    const hasFavoriteItems = favoriteItemsAmount ? true : false
 
-  renderBlock(
-    'user-block',
-    `
+    renderBlock(
+        'user-block',
+        `
     <div class="header-container">
-      <img class="avatar" src="/img/avatar.png" alt="Wade Warren" />
+      <img class="avatar" src="${avatarLink}" alt="Wade Warren" />
       <div class="info">
-          <p class="name">Wade Warren</p>
+          <p class="name">${userName}</p>
           <p class="fav">
             <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
           </p>
       </div>
     </div>
     `
-  )
+    )
 }
