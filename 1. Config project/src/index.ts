d9260bb2a966754e1cdb765acc4c22efe5ec3e67
frontendLearning-31082022 <MorglHer@yandex.ru>
2024-07-05 11:48:29 +0300

    added new task code

--- 1. Config project/src/index.ts
+++ 1. Config project/src/index.ts
@@ -1,7 +1,30 @@
+import { Book } from './book.js'
+import { books } from './book-collection.js'
 import { renderSearchFormBlock } from './search-form.js'
 import { renderSearchStubBlock } from './search-results.js'
 import { renderUserBlock } from './user.js'
 import { renderToast } from './lib.js'
+
+
+function findSuitableBook (genre: string, pagesLimit: number, multipleRecommendations = true): Book | Book[] {
+  const findAlgorithm = (book: Book) => {
+    return book.genre === genre && book.pagesAmount <= pagesLimit
+  }
+
+  if (multipleRecommendations) {
+    return books.filter(findAlgorithm)
+  } else {
+    return books.find(findAlgorithm)
+  }
+}
+
+const recommendedBook = findSuitableBook('fantasy', 1000)
+
+if (recommendedBook instanceof Book) {
+  console.log(recommendedBook.name)
+} else {
+  console.log(recommendedBook[0].name)
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     renderUserBlock("Ирина","/img/avatar.png",0)
@@ -12,3 +35,4 @@ window.addEventListener('DOMContentLoaded', () => {
         { name: 'Понял', handler: () => { console.log('Уведомление закрыто') } }
     )
 })
+
